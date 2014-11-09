765c18868962fc3d450e0d6ecb2933767e770e65
dapbot <robertdapice@gmail.com>
2014-11-09 15:29:41 +1100

    added custom address

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
 	def home
-		  @lat_lng = cookies[:lat_lng].split("|") if cookies[:lat_lng]
+			if params[:location]
+				location = Geocoder.search(params[:location])[0]
+				@lat_lng = [location.latitude.to_s, location.longitude.to_s]
+			else
+			  @lat_lng = cookies[:lat_lng].split("|") if cookies[:lat_lng]
+			end
 		  if @lat_lng
 		  	@restaurants = Restaurant.open_now.near(@lat_lng, 1)
 		  end

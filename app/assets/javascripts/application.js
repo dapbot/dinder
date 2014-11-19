a4fb61d1fe1f8cfbbb92fc1d8f026008bd87a3f2
dapbot <robertdapice@gmail.com>
2014-11-20 10:36:18 +1100

    second attempt at removing address bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -68,6 +68,8 @@ $(function() {
       stack.createCard(targetElement);
   });
 
+  if(!window.pageYOffset){ hideAddressBar(); }
+
 });
 
 
@@ -77,14 +79,13 @@ function hideAddressBar()
   {
       if(document.height < window.outerHeight)
       {
-          document.body.style.height = (window.outerHeight + 50) + 'px';
+          document.body.style.height = (window.outerHeight + 200) + 'px';
       }
  
       setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
   }
 }
  
-window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
 window.addEventListener("orientationchange", hideAddressBar );
 
 

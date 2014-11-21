f7e5c470a22dac12c740f7c70c5d9a03e636d79f
dapbot <robertdapice@gmail.com>
2014-11-21 18:01:04 +1100

    added image resizing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,6 +50,13 @@ $(function() {
   $(".restaurant").css("height", $(".restaurant").width() + 30)
 
   $(".first_row").css("height", Math.floor($(".restaurant").width() * 2 / 3) - 2);
+  // $(".photo_container").each(function(){
+  //   if($(this).width() > $(this).children("img").first.width()){
+  //     $(this).children("img").css("width", $(this).width())
+  //   } else if($(this).height() > $(this).children("img").first.height()){
+  //     $(this).children("img").css("height", $(this).height())
+  //   }    
+  // })
   // $(".extended").css("height", Math.min($(".image_4").height(), $(".image_5").height(), $(".image_6").height()));
   // if (window.resized_images == false){
   //   $(".first_row").css("height", $(".image_1").height());
@@ -151,3 +158,63 @@ $(function() {
 // window.addEventListener("orientationchange", hideAddressBar );
 
 
+function OnImageLoad(evt) {
+
+    var img = evt.currentTarget;
+
+    // what's the size of this image and it's parent
+    var w = $(img).width();
+    var h = $(img).height();
+    var tw = $(img).parent().parent().width();
+    var th = $(img).parent().parent().height();
+
+    // compute the new size and offsets
+    var result = ScaleImage(w, h, tw, th, false);
+
+    // adjust the image coordinates and size
+    img.width = result.width;
+    img.height = result.height;
+    $(img).css("left", result.targetleft);
+    $(img).css("top", result.targettop);
+}
+
+function ScaleImage(srcwidth, srcheight, targetwidth, targetheight, fLetterBox) {
+
+    var result = { width: 0, height: 0, fScaleToTargetWidth: true };
+
+    if ((srcwidth <= 0) || (srcheight <= 0) || (targetwidth <= 0) || (targetheight <= 0)) {
+        return result;
+    }
+
+    // scale to the target width
+    var scaleX1 = targetwidth;
+    var scaleY1 = (srcheight * targetwidth) / srcwidth;
+
+    // scale to the target height
+    var scaleX2 = (srcwidth * targetheight) / srcheight;
+    var scaleY2 = targetheight;
+
+    // now figure out which one we should use
+    var fScaleOnWidth = (scaleX2 > targetwidth);
+    if (fScaleOnWidth) {
+        fScaleOnWidth = fLetterBox;
+    }
+    else {
+       fScaleOnWidth = !fLetterBox;
+    }
+
+    if (fScaleOnWidth) {
+        result.width = Math.floor(scaleX1);
+        result.height = Math.floor(scaleY1);
+        result.fScaleToTargetWidth = true;
+    }
+    else {
+        result.width = Math.floor(scaleX2);
+        result.height = Math.floor(scaleY2);
+        result.fScaleToTargetWidth = false;
+    }
+    result.targetleft = Math.floor((targetwidth - result.width) / 2);
+    result.targettop = Math.floor((targetheight - result.height) / 2);
+
+    return result;
+}
